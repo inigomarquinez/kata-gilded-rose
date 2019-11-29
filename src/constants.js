e27cdf773e3106a3b31e92912b98a1382ce3b758
Daniel Herrero <danielherrerohernando@gmail.com>
2019-11-29 17:29:21 +0100

    added constants file

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const ITEMS = {
+  AGED_BRIE: "Aged Brie",
+  BACKSTAGE: "Backstage passes to a TAFKAL80ETC concert",
+  SULFURAS: "Sulfuras, Hand of Ragnaros",
+  CONJURED: "Conjured"
+};
+
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+
+module.exports = { ITEMS, MAX_QUALITY, MIN_QUALITY };
--- src/gilded_rose_improved.js
+++ src/gilded_rose_improved.js
@@ -1,36 +1,24 @@
-const ITEMS = {
-  AGED_BRIE: 'Aged Brie',
-  BACKSTAGE: 'Backstage passes to a TAFKAL80ETC concert',
-  SULFURAS: 'Sulfuras, Hand of Ragnaros',
-  CONJURED: 'Conjured'
-};
-
-const MAX_QUALITY = 50;
-const MIN_QUALITY = 0;
-
+const { ITEMS, MAX_QUALITY, MIN_QUALITY } = require("./constants");
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
 }
-
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
 
   updateItemQuality(item) {
-    switch(item.name) {
+    switch (item.name) {
       case ITEMS.AGED_BRIE:
-        item.quality = Math.min((item.sellIn > 0 ? item.quality + 1 : item.quality + 2), MAX_QUALITY);
+        item.quality = Math.min(item.sellIn > 0 ? item.quality + 1 : item.quality + 2, MAX_QUALITY);
         break;
-
       case ITEMS.BACKSTAGE:
-        item.quality = item.sellIn > 0 ? Math.min((item.sellIn > 10 ? item.quality + 1 : (item.sellIn > 5 ? item.quality + 2 : item.quality + 3)), MAX_QUALITY) : MIN_QUALITY;
+        item.quality = item.sellIn > 0 ? Math.min(item.sellIn > 10 ? item.quality + 1 : item.sellIn > 5 ? item.quality + 2 : item.quality + 3, MAX_QUALITY) : MIN_QUALITY;
         break;
-
       default:
         item.quality = Math.max(item.quality - (item.name === ITEMS.CONJURED ? 2 : 1), MIN_QUALITY);
     }
@@ -40,8 +28,8 @@ class Shop {
     return this.items.map(item => {
       if (item.name != ITEMS.SULFURAS) {
         this.updateItemQuality(item);
-        item.sellIn -= 1;}
-
+        item.sellIn -= 1;
+      }
       return item;
     });
   }
@@ -50,4 +38,4 @@ class Shop {
 module.exports = {
   Item,
   Shop
-}
+};
